a359fb54bfdd2fe538fcb88b0128c6aa66fbc763
Nuni <banuni@gmail.com>
2017-08-04 20:48:14 +0300

    Naive enrichment algorithm

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
 import wikiquote
 import random
-
+from graph import Graph
 
 app = Flask(__name__)
+DEFAULT_SIZE = 20
+graph = Graph().load("graph.pickle")
 
 
 @app.route('/')
@@ -25,7 +27,24 @@ def get_quote(person_name):
     quote = random.choice(quotes)
     return quote
 
+@app.route('/enrich_list/')
+def get_enrich_list():
+    """
+    GET request
+    given a list of persons, returns an enriched list
+    """
+    people = request.args.get('people', ["Roy Sommer", "Aviv Ben-Haim"])
+    return enrich_list(people)
 
+
+def enrich_list(people_list, up_to=DEFAULT_SIZE):
+    result = set(people_list)
+    for person in people_list:
+        if len(result) >= up_to:
+            break
+        neighbors = graph.get_sorted_neighbours(person)
+        result.add(neighbors[0])
+    return result
 
 if __name__ == '__main__':
     app.run()
